afb4ce0ee79512c95b163a65b04434168132c2e2
Frederico Ramos <up201907180@up.pt>
2024-12-27 17:21:54 +0000

    fds

--- Node/main.c
+++ Node/main.c
@@ -85,6 +85,9 @@ int main(int argc, char* argv[]) {
                 printf("Connection lost! Attempting to reconnect...\n");
                 mqtt_connect(&client, &conn_opts); // Reconnect if disconnected
             }
+            else {
+                printf("its connected\n");
+            }
             connection_check_timer = 0; // Reset timer
         }
 
