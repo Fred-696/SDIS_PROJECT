b8835713c69540191f22869ecd7cb25e2b8974fc
cefovii <cefovi10@gmail.com>
2024-12-29 21:23:43 +0000

    updated

---
+++ Broker2.0/broker.c
@@ -0,0 +1,229 @@
+#include "broker.h"
+
+//function creates server at local ip and given port
+int create_tcpserver(int *server_fd, struct sockaddr_in *address, int *addrlen) {
+    //create socket
+    if ((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { //IPv4, stream-oriented (TCP)
+        perror("Socket creation failed");
+        return -1;
+    }
+
+    //setup address
+    address->sin_family = AF_INET;            //address family to IPv4
+    address->sin_addr.s_addr = INADDR_ANY;    //accepting connectons on any available interface
+    address->sin_port = htons(BROKER_PORT);   //set port in network byte order
+
+    //bind the socket to the specified address and port
+    if (bind(*server_fd, (struct sockaddr *)address, *addrlen) < 0) { //cast to simple struct sockaddr
+        perror("Socket binding failed");
+        close(*server_fd); //clean up the socket before exiting
+        return -1;
+    }
+
+    //listen for incoming connections
+    if (listen(*server_fd, MAX_CLIENTS) < 0){ 
+        perror("Listening failed\n");
+        close(*server_fd); //clean up the socket before exiting
+        return -1;
+    }
+    printf("Server created sucessfully and listening\n");
+    return 0;
+}
+
+//main loop function, for each thread
+void *client_handler(void *arg) {
+    printf("Thread created\n");
+    thread_data *t_data = (thread_data *)arg; //cast to thread data type again
+    int conn_fd = t_data->conn_fd;
+    session *running_sessions = t_data->running_sessions;
+    free(t_data); //no longer needed, free
+
+    uint8_t buffer[BUFFER_SIZE] = {0};
+
+    // Handle client connection
+    while (1) {
+        ssize_t valread = read(conn_fd, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            printf("Client disconnected: conn_fd: %d\n", conn_fd);
+            close(conn_fd);
+            pthread_exit(NULL);
+        }
+
+        mqtt_pck received_pck = {0};
+        received_pck.conn_fd = conn_fd;
+
+        // Process MQTT packet
+        if (mqtt_process_pck(buffer, received_pck, running_sessions) < 0) {
+            printf("MQTT Process Error\n");
+        }
+
+        usleep(10);
+    }
+    return NULL;
+}
+
+//determine type of packet and process
+int mqtt_process_pck(uint8_t *buffer, mqtt_pck received_pck, session* running_session){
+    //======================Analise Fixed Header 1st byte=======================================//
+    received_pck.flag = buffer[0] & 0x0F;             //0->4 flag
+    received_pck.pck_type = (buffer[0] >> 4) & 0x0F;  //4->7 control packet type
+    
+    //==============================Decode remaining package length=============================//
+    int multiplier = 1;
+    uint8_t encoded_byte;
+    int offset = 1; //start after 1st byte
+    for (int i = 0; i < 4; i++){ //encoded remaining bytes info takes max 4 bytes
+        encoded_byte = buffer[offset + i];
+        received_pck.remaining_len += (encoded_byte & 127) * multiplier;
+        multiplier *= 128;
+        if (multiplier > 128*128*128){
+            printf("Malformed Remaining Length\n");
+            return -1;
+        }
+        if ((encoded_byte & 128) == 0){ //if MSB=0 (128 = b'10000000')
+            offset += i + 1; //to know where variable header starts
+            break;
+        }
+    }
+    printf("Flag: %d || Package Type: %d || Remaining Length: %ld\n", received_pck.flag, received_pck.pck_type, received_pck.remaining_len);
+
+    //=============Determine packet type received from a Client=================//
+    printf("Packet Type: ");
+    switch (received_pck.pck_type)
+    {
+    case 1: //CONNECT
+        printf("CONNECT\n");
+        if (received_pck.flag != 0){ //flag must be 0 for CONNECT
+            printf("Invalid flag for CONNECT\n");
+            return -1;
+        }
+        //fill variable header
+        received_pck.variable_header = malloc(10); // Allocate 10 bytes (CONNECT variable header)
+        if (received_pck.variable_header == NULL) {
+            perror("Failed to allocate memory for variable header");
+            exit(EXIT_FAILURE);
+        }
+        memcpy(received_pck.variable_header, buffer + offset, 10); //copy 10 bytes from buffer starting at offset
+        
+        //compute payload len
+        received_pck.payload_len = received_pck.remaining_len - 10;
+
+        //fill payload
+        received_pck.payload = malloc(received_pck.payload_len); // Allocate 10 bytes (CONNECT variable header)
+        if (received_pck.payload == NULL) {
+            perror("Failed to allocate memory for payload");
+            exit(EXIT_FAILURE);
+        }
+        memcpy(received_pck.payload, buffer + offset + 10, received_pck.payload_len); //copy X bytes from buffer starting after variable header
+
+        return connect_handler(&received_pck, running_session); //interpret connect command
+    
+    case 3: //PUBLISH
+        printf("PUBLISH\n");
+        return -1;
+    
+    case 4: //PUBLISH ACKNOWLEDGE
+        printf("PUBACK\n");
+        return -1;
+
+    case 8: //SUBSCRIBE
+        printf("SUBSCRIBE\n");
+        return -1;
+
+    case 12:
+        printf("PING Request\n");
+        return send_pingresp(&received_pck);
+    default:
+        return -1;
+    }
+    return 0;
+}
+
+//handle(interprets) CONNECT packet
+int connect_handler(mqtt_pck *received_pck, session* running_session){
+    int return_code = 0; 
+    int session_present = 0;
+
+    //check variable header
+    uint8_t expected_protocol[8] = {0x00, 0x04, 0x4D, 0x51, 0x54, 0x54, 0x04, 0x02};
+    if (memcmp(received_pck->variable_header, expected_protocol, 8) != 0){
+        printf("Invalid protocol\n");
+        return_code = 1;
+    }
+    int keepalive = received_pck->variable_header[9];
+    
+    //Check payload
+    int id_len = (received_pck->payload[0] << 8)  | received_pck->payload[1];
+
+    char* client_id = malloc(id_len);
+    if (client_id == NULL) {
+        perror("Failed to allocate memory for client id");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(client_id, received_pck->payload + 2, id_len);
+
+    //check if client_id exists in any session
+    int session_idx = -1;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (running_session[i].client_id != NULL) {
+            //compare existing session client_id with the received client_id
+            if (strcmp(running_session[i].client_id, client_id) == 0) {
+                printf("Ongoing session found for Client_ID: %s at index %d\n", client_id, i);
+                session_idx = i;
+                session_present = 1; // Mark session as present
+                break;
+            }
+        } 
+        else if (session_idx == -1) {
+            //save first available slot for a new session
+            session_idx = i;
+        }
+    }
+    //associate client info with session
+    running_session[session_idx].client_id = client_id;
+    running_session[session_idx].conn_fd = received_pck->conn_fd;
+    running_session[session_idx].keepalive = keepalive;
+
+    printf("Valid Protocol || Keepalive: %d || Client_ID: %s || SessionIdx: %d\n", keepalive, client_id, session_idx);
+
+    //assign the new connection to the corresponding session
+    return send_connack(&running_session[session_idx], return_code, session_present);
+}
+
+//Prepares and sends connack package
+int send_connack(session* running_session, int return_code, int session_present) {
+    uint8_t connack_packet[4];
+
+    //fixed Header
+    connack_packet[0] = 0x20; // MQTT Control Packet (0010) || Reserved(0000)
+    connack_packet[1] = 0x02; // Remaining Length (0000) || (0010)
+
+    //variable Header
+    connack_packet[2] = session_present & 0x01; // Reserved(0000) || SessionPresent(which is 1 or 0)
+    connack_packet[3] = return_code; //Connect Return Code (only 0x00 or 0x01)
+    if (send(running_session->conn_fd, connack_packet, sizeof(connack_packet), 0) < 0){
+        printf("Failed to send CONNACK\n");
+        return -1;
+    }
+    printf("CONNACK sent successfully\n");
+    return 0;
+}
+
+//Sends PingResp package(no need for handler before)
+int send_pingresp(mqtt_pck *received_pck) {
+    uint8_t ping_packet[2] = {0};
+    ping_packet[0] = 0xd0; // PINGRESP fixed header
+    ping_packet[1] = 0x00; // Remaining length
+    if (send(received_pck->conn_fd, ping_packet, sizeof(ping_packet), 0) < 0) {
+        perror("Failed to send PING packet");
+        return -1;
+    }
+
+    printf("PINGRESP sent successfully\n");
+    return 0;
+}
+
+
+
+
+int send_pck(mqtt_pck *package, session* running_session);
---
+++ Broker2.0/main.c
@@ -0,0 +1,47 @@
+#include "broker.h"
+//DEPOIS adicionar funcao para dar free da memoria alocado por malloc
+
+int main() {
+    int server_fd;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    session running_sessions[MAX_CLIENTS] = {0};
+
+    if (create_tcpserver(&server_fd, &address, &addrlen) < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        int conn_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (conn_fd < 0) {
+            perror("Connection accept error");
+            continue;
+        }
+        printf("New connection: conn_fd = %d\n", conn_fd);
+
+        //allocate memory for thread data
+        thread_data *t_data = (thread_data *)malloc(sizeof(thread_data)); //memory size, then cast to needed type
+        if (!t_data) {
+            perror("Malloc failed");
+            close(conn_fd);
+            continue;
+        }
+
+        t_data->conn_fd = conn_fd;
+        t_data->running_sessions = running_sessions;
+
+        //create a new thread for the client
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, client_handler, (void *)t_data) != 0) { //cast to void type
+            perror("Thread creation failed");
+            free(t_data);
+            close(conn_fd);
+            continue;
+        }
+
+        // Detach the thread so it cleans up automatically when done
+        pthread_detach(thread_id);
+    }
+
+    return 0;
+}
